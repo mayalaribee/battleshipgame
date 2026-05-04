8b229832d268cb42ff59e2e5c2e8e06d181b4e60
mayalaribee <mayalaribee@gmail.com>
2026-05-03 22:33:08 -0400

    Update outline.py

---
+++ outline.py
@@ -0,0 +1,6 @@
+how to set up board:
+- create a board 
+- transverse across that board in any way possible
+- for each block in the transversal, make it a seperate unit
+- then make an option to place a boat horizontally or vertically at any location
+- then use chat to visualize board 
