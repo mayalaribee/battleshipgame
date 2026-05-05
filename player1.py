5165c31ca8b1a4cf91825aa552d7063807779e8d
mayalaribee <mayalaribee@gmail.com>
2026-05-05 16:43:15 -0400

    Update player1.py

--- player1.py
+++ player1.py
@@ -1,40 +1,73 @@
-from socket32 import create_new_socket
-import player_choice 
+from player_setup import Board
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
+PORT = 65535      # The port used by the server
 def main():
     print('## Welcome to Battleship!')
+    from socket32 import create_new_socket
 
     with create_new_socket() as s:
+
         s.bind(HOST, PORT)
         s.listen()
         print("Server started.")
-        import Player2 as rlib
+        player1_board = Board(10)
+        player2_board = Board(10)
+
+
         conn2player, addr = s.accept()
         print('Connected by', addr)
-    with conn2player:
-        while True:
-            play = conn2player.recv()
-            if choice == '':
-                break
-            player_choice = rlib.#function that defines how the player makes their choice
-            conn2player.sendall(player_choice)
+        conn2player.sendall("SETUP")
+        var = conn2player.recv()
+        if var != "READY":
+            print("Client not ready")
+            return
+        player1_board.ship_place()
+        player1_board.ship_place()
+        turn = 1
+        with conn2player:
+            while True:
+                if turn == 1:
+                    location_row = int(input('Row #:'))
+                    location_col = int(input('Column #:'))
 
-        #create my game loop
-while True:
-    small_ship = '***'
-    large_ship = '*****'
-    if ship_choice == input('Please choose your first ship you would like to place:'):
-        break
-    if input != 'small_ship' or 'large_ship':
-        print('Value must be a ship name, please try again')
+                    conn2player.sendall((f"{location_row},{location_col}"))
+                    play = conn2player.recv()
+                    if not play:
+                        break
+                    try:
+                        location_row = play.split(",")[0]
+                        location_col = play.split(",")[1]
+                        location_row = int(location_row)
+                        location_col = int(location_col)
+                    except:
+                        continue
+                    result = player2_board.mask(location_row, location_col)
+                    player2_board.display()
+                    conn2player.sendall(f"{location_row},{location_col},{result}")
+                    print(result)
+                    turn = 2
+                else:
+                    play = conn2player.recv()
+                    if not play:
+                        break
+                    try:
+                        location_row, location_col, result = play.split(",")
+                        location_row = int(location_row)
+                        location_col = int(location_col)
+                    except:
+                        continue
+                    if result == "hit":
+                        player1_board.guess_board[location_row][location_col] = "X"
+                        print("Hit!")
+                    else:
+                        player1_board.guess_board[location_row][location_col] = "O"
+                        print("Miss!")
+                    player1_board.guess_board.display()
+                    print(result)
 
 
-        s.sendall(str(guess))
-        response = s.recv()
-        print(response)
-
+                    turn = 1
 if __name__ == '__main__':
+
     main()
